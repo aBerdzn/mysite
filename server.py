63f0abcdbeb3dcb1204d7da481146fc7cbff2d9e
aBerdzn <50905529+aBerdzn@users.noreply.github.com>
2019-05-22 14:34:50 +0300

    Update server.py

--- server.py
+++ server.py
@@ -23,6 +23,10 @@ def index():
 def api_test():
     return {"test_passed": True}
 
+@route('/static/<filename>')
+def send_css(filename):
+    return static_file(filename, root='static/')
+
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
